ce9c079188dd008788de0648a46adebe937c6e1a
Adeeb Abubacker <adeebabu655@gmail.com>
2024-02-29 16:32:39 +0530

    initial commit

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      "I' m Prithvi Pramodh, a Game developer, UI designer, and Unreal Engine enthusiast.";
+
+  static const String aboutMeDetail =
+      "Hey there, I'm Prithvi Pramodh, a game developer with a strong foundation in Computer Science from KTU. Currently, I'm immersed in the world of gaming as an Unreal Engine Developer at Cyberfort Technologies, where I bring virtual worlds to life. Before diving into Unreal Engine, I sharpened my skills in game development while working with Unity. During my tenure, I crafted a diverse range of games, each one a testament to my passion and creativity in the field.My journey in game development is fueled by a relentless pursuit of innovation and a desire to push the boundaries of what's possible. I'm excited to continue my exploration in this dynamic industry, eager to create immersive experiences that captivate and inspire players around the globe.";
+}
